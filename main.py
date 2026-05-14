21841efe0d93fe285893604a909e96cfc8b0f68b
Nikolay <fake@email.com>
2026-05-14 15:08:45 +0300

    Version2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import time
 
 app = FastAPI()
 
+request_count = 0
 
 @app.get("/")
 def root():
-    return {"version": "1.0"}
-
+    return {"version": "2.0"}
 
 @app.get("/time")
 def get_time():
+    global request_count
+    request_count += 1
     return {
         "time": int(time.time())
     }
+
+@app.get("/metrics")
+def get_metrics():
+    return {"count": request_count}
