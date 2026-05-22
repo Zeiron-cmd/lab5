82c33aebb65b745a67ad3b1464f9871be50780a8
Zeiron-cmd <timuvasilij@gmail.com>
2026-05-22 11:48:00 +0300

    Metric test

--- test_app.py
+++ test_app.py
@@ -11,3 +11,24 @@ def test_get_time():
     assert "time" in data
     assert isinstance(data["time"], int)
     assert data["time"] > 0
+
+def test_get_metrics():
+    response = client.get("/metrics")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert "count" in data
+    assert isinstance(data["count"], int)
+
+
+def test_metrics_count_increases_after_time_request():
+    before = client.get("/metrics").json()["count"]
+
+    response = client.get("/time")
+    assert response.status_code == 200
+
+    after = client.get("/metrics").json()["count"]
+
+    assert after == before + 1
